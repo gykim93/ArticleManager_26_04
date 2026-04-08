96a74af8fa397f023c4352e6a585af23bfe3055a
gykim93 <gykim1993@gmail.com>
2026-04-08 10:32:22 +0900

    게시글 작성(진행중)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("== 프로그램 시작 ==");
     Scanner sc = new Scanner(System.in);
+
+    int lastArticleId = 0;
+
+    List<Article> articles = new ArrayList<>();
 
     while (true) {
       System.out.print("명령어 ) ");
@@ -17,9 +23,28 @@ public class Main {
         System.out.println("명령어를 입력해주세요");
         continue;
       }
+
+
+      if (cmd.equals("article write")) {
+        System.out.println("== 게시글 작성 ==");
+        int id = lastArticleId + 1;
+        System.out.print("제목 : ");
+        String title = sc.nextLine().trim();
+
+        System.out.print("내용 : ");
+        String body = sc.nextLine().trim();
+
+        System.out.printf("%d번 글이 작성되었습니다.\n", id);
+        lastArticleId++;
+      }
     }
+
+
 
     System.out.println("== 프로그램 종료 ==");
     sc.close();
   }
+}
+class Article {
+
 }
